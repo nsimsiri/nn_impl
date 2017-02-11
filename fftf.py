9d28235c2807ae18dcc098fb6d33509dad699f1b
bladestery <benbeyblade@hotmail.com>
2017-02-11 21:36:45 +0700

    wtf

--- fftf.py
+++ fftf.py
@@ -85,10 +85,10 @@ def training(loss, learning_rate):
   # Create the gradient descent optimizer with the given learning rate.
   optimizer = tf.train.GradientDescentOptimizer(learning_rate)
   # Create a variable to track the global step.
-  global_step = tf.Variable(0, name='global_step', trainable=False)
+  #global_step = tf.Variable(0, name='global_step', trainable=False)
   # Use the optimizer to apply the gradients that minimize the loss
   # (and also increment the global step counter) as a single training step.
-  train_op = optimizer.minimize(loss, global_step=global_step)
+  train_op = optimizer.minimize(loss)
   return train_op
 
 
